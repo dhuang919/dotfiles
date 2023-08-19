e63153efecf7380674acd72a19d03082478ff595
Derek Huang <derekh919@gmail.com>
2023-08-19 15:10:38 -0400

    wezterm configs

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require "wezterm"
+local config = {}
+
+config.colors = {
+  cursor_bg = "#fff"
+}
+config.default_cwd = wezterm.home_dir .. "/dev/bradfield/derekhuang"
+config.font = wezterm.font "Source Code Pro for Powerline"
+config.initial_cols = 120
+config.initial_rows = 35
+config.term = "wezterm"
+
+return config
