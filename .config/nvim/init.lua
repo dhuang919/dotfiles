3a81269b63aeafc696ade203183328e239202d0d
Derek Huang <derekh919@gmail.com>
2023-08-21 18:41:55 -0400

    lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,7 +23,7 @@ vim.g.wildmenu = true
 local lualine = require("lualine")
 lualine.setup {
   options = {
-    theme = "nightfly",
+    theme = "dracula",
   },
 }
 
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
 local wezterm = require "wezterm"
-local config = {}
+local c = {}
 
-config.colors = {
+c.colors = {
   cursor_bg = "#fff"
 }
-config.adjust_window_size_when_changing_font_size = false
-config.default_cwd = wezterm.home_dir .. "/dev/bradfield/derekhuang"
-config.hide_tab_bar_if_only_one_tab = true
-config.initial_cols = 120
-config.initial_rows = 35
-config.use_dead_keys = false
-config.window_close_confirmation = "NeverPrompt"
+c.adjust_window_size_when_changing_font_size = false
+c.default_cwd = wezterm.home_dir .. "/dev/bradfield/derekhuang"
+c.hide_tab_bar_if_only_one_tab = true
+c.initial_cols = 120
+c.initial_rows = 35
+c.use_dead_keys = false
+c.window_close_confirmation = "NeverPrompt"
 
-return config
+return c
