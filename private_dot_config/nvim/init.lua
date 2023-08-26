5a1b337c04bdfaebd714767ee6f382535787f16b
Derek Huang <derekh919@gmail.com>
2023-08-25 22:39:57 -0400

    nvim: trouble bindings

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -27,8 +27,19 @@ Plug("catppuccin/nvim", {as = "catppuccin"})
 Plug("nvim-treesitter/nvim-treesitter", {['do'] = ":TSUpdate"})
 vim.call("plug#end")
 
+
 local autopairs = require("nvim-autopairs")
 autopairs.setup()
+
+
+-- trouble settings
+local trouble = require("trouble")
+vim.keymap.set("n", "<leader>xx", function() trouble.open() end)
+vim.keymap.set("n", "<leader>xw", function() trouble.open("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() trouble.open("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() trouble.open("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() trouble.open("loclist") end)
+vim.keymap.set("n", "gR", function() trouble.open("lsp_references") end)
 
 
 vim.opt.backspace = {"indent", "eol", "start"}
