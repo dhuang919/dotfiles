6b86a419de9a8c4ec0dd692e866311f7deda0096
dhuang295 <dhuang295@bloomberg.net>
2023-09-04 18:43:54 -0400

    nvim: go deps

--- private_dot_config/nvim/lua/plugins/go.lua
+++ private_dot_config/nvim/lua/plugins/go.lua
@@ -2,9 +2,12 @@ return {
   "ray-x/go.nvim",
 
   dependencies = {
+    "mfussenegger/nvim-dap",
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
     "ray-x/guihua.lua",
+    "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
   },
 
   event = "CmdlineEnter",
