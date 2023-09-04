a7c10ef33b75cbfb7dc588e66aeb77933c277f03
Derek Huang <derekh919@gmail.com>
2023-09-03 22:31:48 -0400

    nvim: git blame

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,6 +1,6 @@
 return {
+  "Vimjas/vim-python-pep8-indent",
   "christoomey/vim-tmux-navigator",
   "tpope/vim-commentary",
   "tpope/vim-surround",
-  "Vimjas/vim-python-pep8-indent",
 }
--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -3,19 +3,31 @@ return {
 
   event = "VeryLazy",
 
-  opts = {
-    options = {
-      theme = "dracula",
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = {
-        {
-          "filename",
-          path = 1,
-        },
+  dependencies = {
+    {
+      "f-person/git-blame.nvim",
+
+      event = "VeryLazy",
+
+      opts = {
+        date_format = "%r",
+        display_virtual_text = 0,
       },
     },
   },
+
+  config = function()
+    local git_blame = require("gitblame")
+    require("lualine").setup({
+      options = {
+        theme = "dracula",
+      },
+      sections = {
+        lualine_c = {
+          { "filename", path = 1 },
+          { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+        },
+      },
+    })
+  end,
 }
