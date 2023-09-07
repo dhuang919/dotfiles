6d4197f0a7ae69dca73aab27d2c455eb936c4754
dhuang295 <dhuang295@bloomberg.net>
2023-09-07 17:22:56 -0400

    wterm: turn off tab close prompt

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,15 +4,26 @@ local c = {}
 c.colors = {
   cursor_bg = "#fff",
 }
+
 c.adjust_window_size_when_changing_font_size = false
+
 if string.find(wezterm.home_dir, "derek", 1, true) then
   c.default_cwd = wezterm.home_dir .. "/dev/bradfield/derekhuang"
 elseif string.find(wezterm.home_dir, "dhuang295", 1, true) then
   c.default_cwd = wezterm.home_dir .. "/Projects/tssr"
 end
+
 c.hide_tab_bar_if_only_one_tab = true
 c.initial_cols = 120
 c.initial_rows = 35
+c.keys = {
+  {
+    key = "w",
+    mods = "CMD",
+    actions = wezterm.action.CloseCurrentTab({ confirm = false }),
+  },
+}
+
 c.use_dead_keys = false
 c.window_close_confirmation = "NeverPrompt"
 c.window_decorations = "TITLE | RESIZE | MACOS_FORCE_DISABLE_SHADOW"
