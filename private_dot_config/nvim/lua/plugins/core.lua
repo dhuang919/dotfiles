9fb11d20e990afc7e273ed342d7d4c71a51609a5
dhuang295 <dhuang295@bloomberg.net>
2023-09-19 14:39:46 -0400

    nvim: update after talking to ayush

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -18,23 +18,42 @@ return {
   {
     "nvim-pack/nvim-spectre",
     event = "VeryLazy",
+    keys = {
+      {
+        '<leader>S',
+        '<cmd>lua require("spectre").toggle()<CR>',
+        desc = "Toggle Spectre",
+      },
+      {
+        '<leader>sw',
+        '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+        desc = "Search current word",
+      },
+      {
+        '<leader>sw',
+        '<esc><cmd>lua require("spectre").open_visual()<CR>',
+        "v",
+        desc = "Search current word",
+      },
+      {
+        '<leader>sp',
+        '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+        desc = "Search on current file",
+      },
+    },
   },
   {
     "kylechui/nvim-surround",
     version = "*",
-    config = function()
-      require("nvim-surround").setup()
-    end
+    config = true,
   },
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    config = true,
     init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
     end,
-    config = function()
-      require("which-key").setup()
-    end
   },
 }
--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -7,8 +7,6 @@ return {
   {
     "echasnovski/mini.comment",
     version = false,
-    config = function()
-      require("mini.comment").setup()
-    end
+    config = true,
   },
 }
