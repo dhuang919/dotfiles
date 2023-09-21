7bfa8a682964730568a63d1d0f3ee1d606a2b17b
Derek Huang <derekh919@gmail.com>
2023-09-21 18:39:58 -0400

    nvim: rm gitblame from statusline and add gitsigns

--- private_dot_config/nvim/lua/plugins/statusline.lua
+++ private_dot_config/nvim/lua/plugins/statusline.lua
@@ -9,11 +9,6 @@ return {
 
       event = "VeryLazy",
 
-      opts = {
-        date_format = "%r",
-        display_virtual_text = 0,
-      },
-
       keys = {
         { "<leader>co", "<cmd>GitBlameOpenCommitURL<cr>" },
         { "<leader>fo", "<cmd>GitBlameOpenFileURL<cr>" },
@@ -21,29 +16,14 @@ return {
     },
   },
 
-  config = function()
-    local git_blame = require("gitblame")
-    local lazy_status = require("lazy.status")
-    require("lualine").setup({
-      options = {
-        theme = "dracula",
+  opts = {
+    options = {
+      theme = "dracula",
+    },
+    sections = {
+      lualine_c = {
+        { "filename", path = 1 },
       },
-      sections = {
-        lualine_c = {
-          { "filename", path = 1 },
-          {
-            git_blame.get_current_blame_text,
-            cond = git_blame.is_blame_text_available,
-          },
-        },
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "ff9e64" },
-          }
-        },
-      },
-    })
-  end,
+    },
+  },
 }
