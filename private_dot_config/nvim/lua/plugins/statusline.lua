0d268142113f5908cdaee215962da94bcfe995f4
dhuang295 <dhuang295@bloomberg.net>
2023-09-22 11:27:09 -0400

    nvim: lualine

--- private_dot_config/nvim/lua/plugins/statusline.lua
+++ private_dot_config/nvim/lua/plugins/statusline.lua
@@ -3,6 +3,25 @@ return {
 
   event = "VeryLazy",
 
+  opts = {
+    options = {
+      theme = "ayu_dark",
+    },
+    sections = {
+      lualine_b = {
+        { "branch" },
+      },
+      lualine_c = {
+        { "filename", path = 1 },
+      },
+    },
+    inactive_sections = {
+      lualine_c = {
+        { "filename", path = 1 },
+      },
+    },
+  },
+
   dependencies = {
     {
       "f-person/git-blame.nvim",
@@ -12,22 +31,6 @@ return {
       keys = {
         { "<leader>co", "<cmd>GitBlameOpenCommitURL<cr>" },
         { "<leader>fo", "<cmd>GitBlameOpenFileURL<cr>" },
-      },
-    },
-  },
-
-  opts = {
-    options = {
-      theme = "ayu_dark",
-    },
-    sections = {
-      lualine_c = {
-        { "filename", path = 1 },
-      },
-    },
-    inactive_sections = {
-      lualine_c = {
-        { "filename", path = 1 },
       },
     },
   },
