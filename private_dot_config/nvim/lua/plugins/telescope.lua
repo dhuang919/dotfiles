0f20f527096f74082d45fe5f393c2257b142e95f
dhuang295 <dhuang295@bloomberg.net>
2024-04-23 14:39:26 -0400

    nvim: add telescope live grep args extension

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -13,12 +13,18 @@ return {
       build = "make",
       event = "VeryLazy",
     },
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      version = "^1.0.0",
+      event = "VeryLazy",
+    },
   },
 
   config = function(_, opts)
     local telescope = require("telescope")
     telescope.setup(opts)
     telescope.load_extension("fzf")
+    telescope.load_extension("live_grep_args")
   end,
 
   opts = {
@@ -31,7 +37,7 @@ return {
 
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+    { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
   },
