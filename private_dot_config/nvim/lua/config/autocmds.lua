d7ffef2987d85ab4b2295bc89048c051598475b4
Derek Huang <derekh919@gmail.com>
2024-06-23 16:48:50 -0400

    nvim: replace nvim-tree with oil, replace catppuccin with tokyonight, and clean up unused(?) autocmd.lua

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,27 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
+-- Auto-refresh files after changes stolen from https://unix.stackexchange.com/a/383044/517031
+autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+  pattern = { "*" },
+})
+
+-- Notification after file change
+autocmd({ "FileChangedShellPost" }, {
+  command = "echohl WarningMsg | echo 'File changed on disk. Buffer reloaded.' | echohl None",
+  pattern = { "*" },
+})
+
+autocmd("BufEnter", {
+  desc = "Don't extend comments to newlines with o/O",
+  group = augroup("AutoComment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:append("r")
+    vim.opt_local.formatoptions:remove("c")
+    vim.opt_local.formatoptions:remove("o")
+  end,
+})
 
 autocmd("BufWritePre", {
   desc = "Auto-format go",
--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,14 @@
 return {
   {
-    "catppuccin/nvim",
-
-    name = "catppuccin",
-
-    opts = {
-      flavour = "mocha",
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
-        },
-      },
-    },
-
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin")
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+      })
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
