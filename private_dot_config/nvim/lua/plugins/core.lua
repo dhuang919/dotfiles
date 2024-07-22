b34167a22c13f62b5170dd8c6093338632fe3c6e
dhuang295 <dhuang295@bloomberg.net>
2024-07-22 10:24:02 -0400

    nvim: replace nvim-tree icons with mini.icons

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -84,7 +84,7 @@ return {
   },
   {
     "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
     config = function()
       require("oil").setup({
         keymaps = {
