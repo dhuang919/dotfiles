9ab76a0ab6e9b23ca2e85b50a013119c8d8f95df
dhuang295 <dhuang295@bloomberg.net>
2024-09-05 11:34:37 -0400

    pre-commit autoupdate, zsh for lcldevrc, and nvim spectre to fix -E

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -39,6 +39,20 @@ return {
   {
     "nvim-pack/nvim-spectre",
     event = "VeryLazy",
+    config = function()
+      require("spectre").setup({
+        replace_engine = {
+          ["sed"] = {
+            cmd = "sed",
+            args = {
+              "-i",
+              "",
+              "-E",
+            },
+          },
+        },
+      })
+    end,
     keys = {
       {
         "<leader>S",
