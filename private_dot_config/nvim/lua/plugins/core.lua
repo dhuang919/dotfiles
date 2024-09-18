b33cbf2c9770605a6496dd0e1bef08d8150c2fdf
Derek Huang <derekh919@gmail.com>
2024-09-17 22:56:37 -0400

    nvim: move mini.lua into core.lua

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "echasnovski/mini.splitjoin",
+    event = "VeryLazy",
+    version = false,
+    config = true,
+  },
+  {
+    "echasnovski/mini.comment",
+    version = false,
+    config = true,
+  },
+  {
     "kdheepak/lazygit.nvim",
     cmd = {
       "LazyGit",
