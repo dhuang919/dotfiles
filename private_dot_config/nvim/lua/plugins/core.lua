5670a2670b5fbc2493239d3d3a32c6c02604b237
Derek Huang <derekh919@gmail.com>
2024-09-17 22:58:56 -0400

    nvim: move statusline.lua into core.lua

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,5 +1,27 @@
 return {
   {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "ayu_dark",
+      },
+      sections = {
+        lualine_b = {
+          { "branch" },
+        },
+        lualine_c = {
+          { "filename", path = 3 },
+        },
+      },
+      inactive_sections = {
+        lualine_c = {
+          { "filename", path = 3 },
+        },
+      },
+    },
+  },
+  {
     "echasnovski/mini.splitjoin",
     event = "VeryLazy",
     version = false,
