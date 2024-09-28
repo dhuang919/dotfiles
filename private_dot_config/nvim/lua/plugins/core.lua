d7fc0632f4deb709fa1a9d7cc9c316a417ecdc71
Derek Huang <derekh919@gmail.com>
2024-09-27 22:09:01 -0400

    nvim: rm markview.nvim

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,10 +1,10 @@
 return {
   {
-    'MeanderingProgrammer/render-markdown.nvim',
+    "MeanderingProgrammer/render-markdown.nvim",
     opts = {},
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons',
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
     },
   },
   {
@@ -78,15 +78,6 @@ return {
     event = "InsertEnter",
     opts = {
       check_ts = true,
-    },
-  },
-  {
-    "OXY2DEV/markview.nvim",
-    enabled = false,
-    lazy = false,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
     },
   },
   {
