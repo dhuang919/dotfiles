4155d24b127fe01e10adb9c9fb78104b834a4874
dhuang295 <dhuang295@bloomberg.net>
2024-10-09 16:22:07 -0400

    nvim: treesitter prefer_git true

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
   event = "VeryLazy",
   build = ":TSUpdate",
   config = function()
+    require("nvim-treesitter.install").prefer_git = true
     require("nvim-treesitter.configs").setup({
       auto_install = true,
       ensure_installed = {
