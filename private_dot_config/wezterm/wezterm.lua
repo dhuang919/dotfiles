65e7b43df40dba18bd03ec5e89e33f21b7894497
dhuang295 <dhuang295@bloomberg.net>
2024-10-17 15:07:32 -0400

    wezterm: try tab bar bottom

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -121,6 +121,7 @@ c.hide_tab_bar_if_only_one_tab = true
 c.initial_cols = 120
 c.initial_rows = 35
 c.scrollback_lines = 5000
+c.tab_bar_at_bottom = true
 c.use_dead_keys = false
 c.window_close_confirmation = "NeverPrompt"
 c.window_decorations = "TITLE | RESIZE | MACOS_FORCE_DISABLE_SHADOW"
