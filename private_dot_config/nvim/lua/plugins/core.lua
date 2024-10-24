c5324b8f9822c356cb7ba760432cb98696c92a01
dhuang295 <dhuang295@bloomberg.net>
2024-10-24 13:00:23 -0400

    nvim: fix oil mini.icons

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -176,7 +176,7 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = {
-      "echasnovski/mini.icons",
+      { "echasnovski/mini.icons", opts = {} },
     },
     config = function()
       require("oil").setup({
