626209990a2456cd46e16f649caeb0325682c8d8
dhuang295 <dhuang295@bloomberg.net>
2024-12-01 15:03:01 -0500

    nvim: rm mini.ai custom config

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -1,29 +1,9 @@
 return {
   {
     "echasnovski/mini.ai",
-    dependencies = { "echasnovski/mini.extra", version = "*" },
     event = "VeryLazy",
     version = "*",
-    config = function()
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      local ai = require("mini.ai")
-      ai.setup({
-        n_lines = 500,
-        custom_textobjects = {
-          ["%"] = "",
-          s = ai.gen_spec.treesitter({ -- code block
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-          i = require("mini.extra").gen_ai_spec.indent(),
-          g = require("mini.extra").gen_ai_spec.buffer(),
-        },
-      })
-    end,
+    config = true,
   },
   {
     "echasnovski/mini.comment",
