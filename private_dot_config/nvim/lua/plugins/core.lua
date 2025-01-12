013fa66aab57ce9a71f934785a2ccd0aa1f48dc3
dhuang295 <dhuang295@bloomberg.net>
2025-01-12 17:42:29 -0500

    nvim: auto-session keys

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -207,8 +207,17 @@ return {
   {
     "rmagatti/auto-session",
     lazy = false,
-    opts = {
-      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    keys = {
+      { "<leader>se", "<cmd>SessionSearch<CR>", desc = "Session search" },
+      { "<leader>ss", "<cmd>SessionSave<CR>", desc = "Save session" },
+      { "<leader>sr", "<cmd>SessionRestore<CR>", desc = "Restore session" },
     },
+    config = function()
+      vim.o.sessionoptions =
+        "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+      require("auto-session").setup({
+        suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      })
+    end,
   },
 }
