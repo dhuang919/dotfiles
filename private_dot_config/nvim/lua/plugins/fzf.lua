c4c707fd877239d630d5e51647e3351ec4729c57
Derek Huang <derekh919@gmail.com>
2025-02-03 13:35:04 -0500

    nvim: fzf-lua grep_last

--- private_dot_config/nvim/lua/plugins/fzf.lua
+++ private_dot_config/nvim/lua/plugins/fzf.lua
@@ -14,8 +14,9 @@ return {
   keys = {
     { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "fzf files" },
     { "<leader>fo", "<cmd>FzfLua oldfiles<cr>", desc = "fzf old files" },
-    { "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "fzf live grep" },
+    { "<leader>fg", "<cmd>FzfLua live grep<cr>", desc = "fzf live grep" },
     { "<leader>fl", "<cmd>FzfLua grep<cr>", desc = "fzf grep" },
+    { "<leader>gl", "<cmd>FzfLua grep last<cr>", desc = "fzf grep last" },
     { "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "fzf buffers" },
     { "<leader>gc", "<cmd>FzfLua git_commits<cr>", desc = "fzf git commit log" },
   },
