83c206e913ae71a7911b6f3b8bfdab530ca1e6f5
Derek Huang <derekh919@gmail.com>
2025-03-14 11:31:10 -0400

    nvim: move LspAttach autocmd to lsp.lua

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -16,6 +16,30 @@ return {
       require("mason").setup()
       local mason_lspcfg = require("mason-lspconfig")
       local lspconfig = require("lspconfig")
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions",
+        callback = function(event)
+          local lspopts = { buffer = event.buf }
+          vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", lspopts)
+          vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", lspopts)
+          vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", lspopts)
+          vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", lspopts)
+          vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", lspopts)
+          vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", lspopts)
+          -- disabling because of conflict with leap
+          -- vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", lspopts)
+          vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", lspopts)
+          vim.keymap.set(
+            { "n", "x" },
+            "<F3>",
+            "<cmd>lua vim.lsp.buf.format({async = true})<cr>",
+            lspopts
+          )
+          vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", lspopts)
+        end,
+      })
+
       mason_lspcfg.setup({
         ensure_installed = {
           "clangd",
