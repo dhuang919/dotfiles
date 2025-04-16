3781462890d41bcd1de5c1e10607a4616b710ac9
Derek Huang <derekh919@gmail.com>
2025-04-16 10:07:00 -0400

    nvim: fix precognition toggle

--- private_dot_config/nvim/lua/plugins/precognition.lua
+++ private_dot_config/nvim/lua/plugins/precognition.lua
@@ -2,6 +2,6 @@ return {
   "tris203/precognition.nvim",
   opts = {},
   keys = {
-    { "<leader>pc", "<cmd>Precognition toggle", desc = "Precognition toggle" },
+    { "<leader>pc", "<cmd>Precognition toggle<cr>", desc = "Precognition toggle" },
   },
 }
