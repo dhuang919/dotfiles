7bd27466edecf499451b6d19d667f58b9a1d858c
Derek Huang <derekh919@gmail.com>
2025-04-30 11:00:32 -0400

    nvim: fix various-testobjs

--- private_dot_config/nvim/lua/plugins/nvim-various-textobjs.lua
+++ private_dot_config/nvim/lua/plugins/nvim-various-textobjs.lua
@@ -1,9 +1,14 @@
 return {
   "chrisgrieser/nvim-various-textobjs",
-  event = "VeryLazy",
+  lazy = false,
   opts = {
     keymaps = {
+      -- conflicts with mini.ai
+      -- ai/ii: indentation
+      -- aq/iq: anyQuote (mini.ai q)
+      -- ao/io: anyBracket (mini.ai b)
       disabledDefaults = { "ai", "ii", "aq", "iq", "ao", "io" },
+      useDefaults = true,
     },
   },
 }
