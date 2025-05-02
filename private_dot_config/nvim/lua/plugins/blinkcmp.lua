6efef9e15c336dff518224acd37c1ef591d5d839
Derek Huang <derekh919@gmail.com>
2025-05-02 14:35:59 -0400

    nvim: blink.cmp 'enter' keymap preset

--- private_dot_config/nvim/lua/plugins/blinkcmp.lua
+++ private_dot_config/nvim/lua/plugins/blinkcmp.lua
@@ -7,7 +7,12 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default" },
+    keymap = { preset = "enter" },
+    completion = {
+      list = {
+        selection = { preselect = false, auto_insert = false },
+      },
+    },
     appearance = {
       use_nvim_cmp_as_default = false,
       nerd_font_variant = "mono",
