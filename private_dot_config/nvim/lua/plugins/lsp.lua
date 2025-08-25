76786371bb6662d4070063aee551dafee6cd8585
Derek Huang <derekh919@gmail.com>
2025-08-24 22:12:43 -0400

    Revert "nvim: clean up lsp setup and fix blink.cmp integration"
    This reverts commit e4687ee6d92b5afaffe3917485319a8998ff1411.

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,86 +1,3 @@
-local servers = {
-  bashls = {},
-  clangd = {},
-  docker_compose_language_service = {},
-  dockerls = {},
-  gopls = {},
-  jsonls = {},
-  lua_ls = {
-    -- taken from https://lsp-zero.netlify.app/docs/guide/neovim-lua-ls.html
-    settings = {
-      Lua = {
-        telemetry = { enable = false },
-      },
-    },
-    on_init = function(client)
-      local join = vim.fs.joinpath
-      local path = client.workspace_folders[1].name
-
-      -- don't do anything if there is project local config
-      if
-        vim.uv.fs_stat(join(path, ".luarc.json"))
-        or vim.uv.fs_stat(join(path, ".luarc.jsonc"))
-      then
-        return
-      end
-
-      -- apply neovim specific settings
-      local runtime_path = vim.split(package.path, ";")
-      table.insert(runtime_path, join("lua", "?.lua"))
-      table.insert(runtime_path, join("lua", "?", "init.lua"))
-
-      local nvim_settings = {
-        runtime = {
-          version = "LuaJIT",
-          path = runtime_path,
-        },
-        diagnostics = {
-          -- get the language server to recognize the `vim` global
-          globals = { "vim" },
-        },
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            -- make the server aware of neovim runtime files
-            vim.env.VIMRUNTIME,
-            vim.fn.stdpath("config"),
-          },
-        },
-      }
-
-      client.config.settings.Lua =
-        vim.tbl_deep_extend("force", client.config.settings.Lua, nvim_settings)
-    end,
-  },
-  pyright = {
-    settings = {
-      pyright = {
-        disableOrganizeImports = true, -- use ruff
-      },
-      python = {
-        analysis = {
-          diagnosticSeverityOverrides = {
-            reportMissingImports = "none",
-          },
-        },
-      },
-    },
-  },
-  ruff = {
-    capabilities = {
-      general = {
-        -- ruff was using utf-8 while pyright was using utf-16 (preferred)
-        -- https://github.com/astral-sh/ruff/issues/14483
-        positionEncodings = { "utf-16" },
-      },
-    },
-  },
-  rust_analyzer = {},
-  sqlls = {},
-  ts_ls = {},
-  yamlls = {},
-}
-
 local function jumpWithVirtLineDiags(jumpCount)
   -- https://www.reddit.com/r/neovim/comments/1jm5atz/comment/mk9w6v0
   pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
@@ -108,6 +25,7 @@ end
 return {
   {
     "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       { "williamboman/mason.nvim" },
       { "williamboman/mason-lspconfig.nvim" },
@@ -118,7 +36,7 @@ return {
       -- this will avoid an annoying layout shift in the screen
       vim.opt.signcolumn = "yes"
     end,
-    config = function()
+    config = function(_, opts)
       require("mason").setup()
       local mason_lspcfg = require("mason-lspconfig")
       local lspconfig = require("lspconfig")
@@ -169,19 +87,123 @@ return {
           vim.keymap.set("n", "<leader>gE", function()
             jumpWithVirtLineDiags(-1)
           end, { desc = "lsp prev diagnostic", buffer = event.buf })
+          local lspopts = { buffer = event.buf }
+          vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", lspopts)
+          vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", lspopts)
+          vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", lspopts)
+          vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", lspopts)
+          vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", lspopts)
+          vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", lspopts)
+          vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", lspopts)
         end,
       })
 
       mason_lspcfg.setup({
-        ensure_installed = vim.tbl_keys(servers),
+        ensure_installed = {
+          "clangd",
+          "docker_compose_language_service",
+          "dockerls",
+          "gopls",
+          "jsonls",
+          "lua_ls",
+          "pyright",
+          "ruff",
+          "rust_analyzer",
+          "sqlls",
+          "ts_ls",
+          "yamlls",
+        },
         handlers = {
+          -- this first function is the "default handler"
+          -- it applies to every language server without a "custom handler"
           function(server_name)
-            local server = servers[server_name]
-            server.capabilities = require("blink.cmp").get_lsp_capabilities(server.capabilities)
-            lspconfig[server_name].setup(server)
+            lspconfig[server_name].setup({})
+          end,
+          lua_ls = function()
+            lspconfig.lua_ls.setup({
+              -- taken from https://lsp-zero.netlify.app/docs/guide/neovim-lua-ls.html
+              settings = {
+                Lua = {
+                  telemetry = { enable = false },
+                },
+              },
+              on_init = function(client)
+                local join = vim.fs.joinpath
+                local path = client.workspace_folders[1].name
+
+                -- don't do anything if there is project local config
+                if
+                  vim.uv.fs_stat(join(path, ".luarc.json"))
+                  or vim.uv.fs_stat(join(path, ".luarc.jsonc"))
+                then
+                  return
+                end
+
+                -- apply neovim specific settings
+                local runtime_path = vim.split(package.path, ";")
+                table.insert(runtime_path, join("lua", "?.lua"))
+                table.insert(runtime_path, join("lua", "?", "init.lua"))
+
+                local nvim_settings = {
+                  runtime = {
+                    version = "LuaJIT",
+                    path = runtime_path,
+                  },
+                  diagnostics = {
+                    -- get the language server to recognize the `vim` global
+                    globals = { "vim" },
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                    library = {
+                      -- make the server aware of neovim runtime files
+                      vim.env.VIMRUNTIME,
+                      vim.fn.stdpath("config"),
+                    },
+                  },
+                }
+
+                client.config.settings.Lua =
+                  vim.tbl_deep_extend("force", client.config.settings.Lua, nvim_settings)
+              end,
+            })
+          end,
+          pyright = function()
+            lspconfig.pyright.setup({
+              settings = {
+                pyright = {
+                  disableOrganizeImports = true, -- use ruff
+                },
+                python = {
+                  analysis = {
+                    diagnosticSeverityOverrides = {
+                      reportMissingImports = "none",
+                    },
+                  },
+                },
+              },
+            })
+          end,
+          ruff = function()
+            lspconfig.ruff.setup({
+              capabilities = {
+                general = {
+                  -- ruff was using utf-8 while pyright was using utf-16 (preferred)
+                  -- https://github.com/astral-sh/ruff/issues/14483
+                  positionEncodings = { "utf-16" },
+                },
+              },
+            })
           end,
         },
       })
+      -- blink.cmp setup
+      for server, config in pairs(opts.servers or {}) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
     end,
   },
 }
