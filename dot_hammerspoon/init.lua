00e18c6a0d789fc2baea65c2d2a68dcf361794ab
Derek Huang <derekh919@gmail.com>
2025-08-28 15:34:22 -0400

    hs: move laptop to horizontal

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -2,10 +2,10 @@ local s = require("screens")
 local external_connected = s.screens["HORIZONTAL"] ~= nil or s.screens["VERTICAL"] ~= nil
 
 local ratios = {
-  bbvpn = { top = 0.5, left = 0.005, height = 0.4, width = 0.25 },
-  calendar = { top = 0.07, left = 0.33, height = 0.6, width = 0.62 },
-  chrome_lptp = { top = 0, left = 0.1, height = 1, width = 0.8 },
-  chrome_ext = { top = 0.19, left = 0, height = 0.378, width = 1 },
+  bbvpn = { top = 0.647, left = 0.008, height = 0.341, width = 0.160 },
+  calendar = { top = 0.046, left = 0.603, height = 0.378, width = 0.383 },
+  chrome_horizontal = { top = 0.078, left = 0.506, height = 0.720, width = 0.472 },
+  chrome_vertical = { top = 0.19, left = 0, height = 0.378, width = 1 },
   wezterm = {
     top = external_connected and 0.568 or 0,
     left = 0,
@@ -13,20 +13,19 @@ local ratios = {
     width = external_connected and 1 or 0.58,
   },
   mail = { top = 0.03, left = 0.004, height = 0.77, width = 0.8 },
-  slack_ext = { top = 0.392, left = 0.004, height = 0.579, width = 0.493 },
-  slack_lptp = { top = 0.063, left = 0.02, height = 0.85, width = 0.85 },
-  spotify = { top = 0.21, left = 0.01, height = 0.77, width = 0.7 },
-  obsidian = { top = 0.145, left = 0.17, height = 0.82, width = 0.8 },
-  messages = { top = 0.450, left = 0.439, height = 0.535, width = 0.544 },
+  slack_horizontal = { top = 0.071, left = 0.021, height = 0.871, width = 0.467 },
+  spotify = { top = 0.463, left = 0.013, height = 0.500, width = 0.426 },
+  obsidian = { top = 0.040, left = 0.438, height = 0.960, width = 0.370 },
+  messages = { top = 0.595, left = 0.668, height = 0.365, width = 0.321 },
 }
 
 hs.hotkey.bind({ "alt", "cmd" }, "w", function()
-  s.moveIfOpen("Spotify", "LAPTOP", ratios.spotify)
-  s.moveIfOpen("bbvpn2", "LAPTOP", ratios.bbvpn)
-  s.moveIfOpen("Calendar", "LAPTOP", ratios.calendar)
-  s.moveIfOpen("Obsidian", "LAPTOP", ratios.obsidian)
-  s.moveIfOpen("Messages", "LAPTOP", ratios.messages)
-  s.moveIfOpen("Slack", "LAPTOP", ratios.slack_lptp)
+  s.moveIfOpen("Spotify", "HORIZONTAL", ratios.spotify)
+  s.moveIfOpen("bbvpn2", "HORIZONTAL", ratios.bbvpn)
+  s.moveIfOpen("Calendar", "HORIZONTAL", ratios.calendar)
+  s.moveIfOpen("Obsidian", "HORIZONTAL", ratios.obsidian)
+  s.moveIfOpen("Messages", "HORIZONTAL", ratios.messages)
+  s.moveIfOpen("Slack", "HORIZONTAL", ratios.slack_horizontal)
   s.moveIfOpen(
     "WezTerm",
     external_connected and "VERTICAL" or "LAPTOP",
@@ -37,7 +36,7 @@ hs.hotkey.bind({ "alt", "cmd" }, "w", function()
   s.moveIfOpen(
     "Google Chrome",
     external_connected and "VERTICAL" or "LAPTOP",
-    external_connected and ratios.chrome_ext or ratios.chrome_lptp,
+    external_connected and ratios.chrome_vertical or ratios.chrome_horizontal,
     false,
     true
   )
@@ -49,11 +48,11 @@ hs.hotkey.bind({ "alt", "cmd" }, "r", function()
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "c", function()
-  s.moveIfOpen("Google Chrome", "LAPTOP", ratios.chrome_lptp, false, true)
+  s.moveIfOpen("Google Chrome", "HORIZONTAL", ratios.chrome_horizontal, false, true)
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "o", function()
-  s.moveIfOpen("WezTerm", "LAPTOP", ratios.obsidian)
+  s.moveIfOpen("WezTerm", "HORIZONTAL", ratios.obsidian)
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "s", function()
