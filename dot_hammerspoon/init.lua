fff0f899680643a50bb66c5e5d101f988d831d50
Derek Huang <derekh919@gmail.com>
2025-08-30 23:14:28 -0400

    hs: bring back slack, obsidian, and chrome laptop ratios and make alt + cmd + o/c conditional

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -4,6 +4,7 @@ local external_connected = s.screens["HORIZONTAL"] ~= nil or s.screens["VERTICAL
 local ratios = {
   bbvpn = { top = 0.647, left = 0.008, height = 0.341, width = 0.160 },
   calendar = { top = 0.046, left = 0.603, height = 0.378, width = 0.383 },
+  chrome_lptp = { top = 0, left = 0.1, height = 1, width = 0.8 },
   chrome_horizontal = { top = 0.078, left = 0.506, height = 0.786, width = 0.472 },
   chrome_vertical = { top = 0.19, left = 0, height = 0.378, width = 1 },
   wezterm = {
@@ -13,9 +14,11 @@ local ratios = {
     width = external_connected and 1 or 0.58,
   },
   mail = { top = 0.03, left = 0.004, height = 0.77, width = 0.8 },
+  slack_lptp = { top = 0.063, left = 0.02, height = 0.85, width = 0.85 },
   slack_horizontal = { top = 0.038, left = 0.020, height = 0.871, width = 0.467 },
   spotify = { top = 0.463, left = 0.013, height = 0.500, width = 0.426 },
-  obsidian = { top = 0.431, left = 0.500, height = 0.552, width = 0.483 },
+  obsidian_lptp = { top = 0.145, left = 0.17, height = 0.82, width = 0.8 },
+  obsidian_horizontal = { top = 0.431, left = 0.500, height = 0.552, width = 0.483 },
   messages = { top = 0.595, left = 0.668, height = 0.365, width = 0.321 },
 }
 
@@ -23,7 +26,7 @@ hs.hotkey.bind({ "alt", "cmd" }, "w", function()
   s.moveIfOpen("Spotify", "HORIZONTAL", ratios.spotify)
   s.moveIfOpen("bbvpn2", "HORIZONTAL", ratios.bbvpn)
   s.moveIfOpen("Calendar", "HORIZONTAL", ratios.calendar)
-  s.moveIfOpen("Obsidian", "HORIZONTAL", ratios.obsidian)
+  s.moveIfOpen("Obsidian", "HORIZONTAL", ratios.obsidian_horizontal)
   s.moveIfOpen("Messages", "HORIZONTAL", ratios.messages)
   s.moveIfOpen("Slack", "HORIZONTAL", ratios.slack_horizontal)
   s.moveIfOpen(
@@ -48,11 +51,21 @@ hs.hotkey.bind({ "alt", "cmd" }, "r", function()
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "c", function()
-  s.moveIfOpen("Google Chrome", "HORIZONTAL", ratios.chrome_horizontal, false, true)
+  s.moveIfOpen(
+    "Google Chrome",
+    external_connected and "HORIZONTAL" or "LAPTOP",
+    external_connected and ratios.chrome_horizontal or ratios.chrome_lptp,
+    false,
+    true
+  )
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "o", function()
-  s.moveIfOpen("WezTerm", "HORIZONTAL", ratios.obsidian)
+  s.moveIfOpen(
+    "WezTerm",
+    external_connected and "HORIZONTAL" or "LAPTOP",
+    external_connected and ratios.obsidian_horizontal or ratios.obsidian_lptp
+  )
 end)
 
 hs.hotkey.bind({ "alt", "cmd" }, "s", function()
