c1298a37bcb8427fd6ecce7f1b54d0341028f4b6
Derek Huang <derekh919@gmail.com>
2025-09-23 18:05:19 -0400

    nvim [navigator]: add alt bindings when ssh'd

--- private_dot_config/nvim/lua/plugins/navigator.lua
+++ private_dot_config/nvim/lua/plugins/navigator.lua
@@ -4,10 +4,21 @@ return {
   opts = {
     auto_save = "all",
   },
-  keys = {
-    { "<C-h>", "<cmd>NavigatorLeft<cr>", desc = "NavigatorLeft" },
-    { "<C-j>", "<cmd>NavigatorDown<cr>", desc = "NavigatorDown" },
-    { "<C-k>", "<cmd>NavigatorUp<cr>", desc = "NavigatorUp" },
-    { "<C-l>", "<cmd>NavigatorRight<cr>", desc = "NavigatorRight" },
-  },
+  keys = function()
+    local keys = {
+      { "<C-h>", "<cmd>NavigatorLeft<cr>", desc = "NavigatorLeft" },
+      { "<C-j>", "<cmd>NavigatorDown<cr>", desc = "NavigatorDown" },
+      { "<C-k>", "<cmd>NavigatorUp<cr>", desc = "NavigatorUp" },
+      { "<C-l>", "<cmd>NavigatorRight<cr>", desc = "NavigatorRight" },
+    }
+    if vim.env.SSH_CLIENT or vim.env.SSH_TTY then
+      vim.list_extend(keys, {
+        { "<M-h>", "<cmd>NavigatorLeft<cr>", desc = "NavigatorLeft" },
+        { "<M-j>", "<cmd>NavigatorDown<cr>", desc = "NavigatorDown" },
+        { "<M-k>", "<cmd>NavigatorUp<cr>", desc = "NavigatorUp" },
+        { "<M-l>", "<cmd>NavigatorRight<cr>", desc = "NavigatorRight" },
+      })
+    end
+    return keys
+  end,
 }
