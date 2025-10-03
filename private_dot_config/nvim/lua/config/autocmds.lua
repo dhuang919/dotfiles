c8a0ffc6a00b9fc25065126bea2adc1f147cb369
Derek Huang <dhuang295@bloomberg.net>
2025-10-03 14:54:04 -0400

    nvim [autocmds]: bring back lsp key bindings

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -115,15 +115,87 @@ autocmd("CursorHold", {
   end,
 })
 
+local function jumpWithVirtLineDiags(jumpCount)
+  -- https://www.reddit.com/r/neovim/comments/1jm5atz/comment/mk9w6v0
+  pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
+
+  vim.diagnostic.jump({ count = jumpCount })
+
+  local initialVirtTextConf = vim.diagnostic.config().virtual_text
+  vim.diagnostic.config({
+    virtual_text = false,
+    virtual_lines = { current_line = true },
+  })
+
+  vim.defer_fn(function() -- deferred to not trigger by jump itself
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      desc = "User(once): Reset diagnostics virtual lines",
+      once = true,
+      group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
+      callback = function()
+        vim.diagnostic.config({ virtual_lines = false, virtual_text = initialVirtTextConf })
+      end,
+    })
+  end, 1)
+end
+
 autocmd("LspAttach", {
   desc = "LSP key bindings",
   callback = function(event)
+    vim.keymap.set(
+      "n",
+      "K",
+      vim.lsp.buf.hover,
+      { desc = "LSP Hover", buffer = event.buf }
+    )
     vim.keymap.set(
       "n",
       "<leader>rn",
       "<cmd>lua vim.lsp.buf.rename()<cr>",
       { desc = "LSP Rename", buffer = event.buf }
     )
+    vim.keymap.set(
+      "n",
+      "gd",
+      vim.lsp.buf.definition,
+      { desc = "LSP Go To Definition", buffer = event.buf }
+    )
+    vim.keymap.set(
+      "n",
+      "gD",
+      vim.lsp.buf.declaration,
+      { desc = "LSP Go To Declaration", buffer = event.buf }
+    )
+    vim.keymap.set(
+      "n",
+      "gi",
+      vim.lsp.buf.implementation,
+      { desc = "LSP Go To Implementation", buffer = event.buf }
+    )
+    vim.keymap.set(
+      "n",
+      "gt",
+      vim.lsp.buf.type_definition,
+      { desc = "LSP Go To Type Definition", buffer = event.buf }
+    )
+    vim.keymap.set(
+      "n",
+      "gr",
+      vim.lsp.buf.references,
+      { desc = "LSP References", buffer = event.buf }
+    )
+    vim.keymap.set(
+      "n",
+      "gs",
+      vim.lsp.buf.signature_help,
+      { desc = "LSP Signature Help", buffer = event.buf }
+    )
+    vim.keymap.set("n", "<leader>ge", function()
+      jumpWithVirtLineDiags(1)
+    end, { desc = "LSP Next Diagnostic", buffer = event.buf })
+    vim.keymap.set("n", "<leader>gE", function()
+      jumpWithVirtLineDiags(-1)
+    end, { desc = "LSP Prev Diagnostic", buffer = event.buf })
   end,
 })
 
