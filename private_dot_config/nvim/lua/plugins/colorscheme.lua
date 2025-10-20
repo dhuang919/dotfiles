7560683683f78f4663f59245372e1d6a3571e57a
Derek Huang <dhuang295@bloomberg.net>
2025-10-19 20:38:54 -0400

    nvim [colorscheme]: replace moonfly w nanode

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
 return {
-  "bluz71/vim-moonfly-colors",
-  name = "moonfly",
-  lazy = false,
+  "KijitoraFinch/nanode.nvim",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("moonfly")
-
-    -- flash.nvim highlight overrides for moonfly
-    vim.api.nvim_set_hl(0, "FlashMatch", { fg = "#080808", bg = "#5fafdf", bold = true }) -- blue
-    vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#080808", bg = "#ff8700", bold = true }) -- orange
-    vim.api.nvim_set_hl(0, "FlashCurrent", { fg = "#080808", bg = "#87d700", bold = true }) -- green
-    vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#444444" }) -- dim gray
+    require("nanode").setup({
+      transparent = false,
+    })
+    vim.cmd.colorscheme("nanode")
   end,
 }
