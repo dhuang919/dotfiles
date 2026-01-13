bd712a263a479ee492e848fc35691da546f314b8
Derek Huang <derekh919@gmail.com>
2026-01-13 14:41:56 -0500

    nvim: update sessionoptions based on healthcheck rec

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -7,7 +7,7 @@ return {
     { "<leader>ss", "<cmd>AutoSession save<CR>", desc = "Save session" },
   },
   config = function()
-    vim.o.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal"
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     require("auto-session").setup({
       auto_save = false,
       suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
