67ae8e6d783680ef6b9a1efde7625957b7fc9467
Derek Huang <derekh919@gmail.com>
2026-04-03 16:11:01 -0400

    nvim [treesitter]: fix/update

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,6 +6,37 @@ return {
   config = function()
     -- Add runtime/queries to runtimepath for fold queries
     vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/runtime")
-    require("nvim-treesitter").setup({ auto_install = true })
+
+    local parsers = {
+      "bash",
+      "c",
+      "cpp",
+      "dockerfile",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "rust",
+      "sql",
+      "toml",
+      "tsx",
+      "typescript",
+      "xml",
+      "yaml",
+    }
+
+    local installed = require("nvim-treesitter.config").get_installed()
+    local missing = vim.tbl_filter(function(lang)
+      return not vim.list_contains(installed, lang)
+    end, parsers)
+
+    if #missing > 0 then
+      vim.cmd("TSInstall " .. table.concat(missing, " "))
+    end
   end,
 }
