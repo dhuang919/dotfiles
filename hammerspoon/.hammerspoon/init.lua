5fa73d59dd1137bb57b3a58eb0b9c8a6bb1687a5
Derek Huang <derekh919@gmail.com>
2026-05-05 14:43:32 -0400

    hs: rearrange work setup

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,21 +5,20 @@ local ratios = {
   bbvpn = { top = 0.647, left = 0.008, height = 0.341, width = 0.160 },
   calendar = { top = 0.046, left = 0.603, height = 0.378, width = 0.383 },
   chrome_lptp = { top = 0, left = 0.1, height = 1, width = 0.8 },
-  chrome_horizontal = { top = 0.078, left = 0.506, height = 0.786, width = 0.472 },
-  chrome_vertical = { top = 0.01, left = 0, height = 0.558, width = 1 },
+  chrome_ext_lptp = { top = 0.078, left = 0.506, height = 0.786, width = 0.472 },
+  chrome_horiz = { top = 0.043, left = 0.342, height = 0.864, width = 0.613 },
   wezterm = {
     top = external_connected and 0.568 or 0,
     left = 0,
     height = external_connected and 0.432 or 0.65,
     width = external_connected and 1 or 0.58,
   },
-  mail = { top = 0.03, left = 0.004, height = 0.77, width = 0.8 },
   slack_lptp = { top = 0.063, left = 0.02, height = 0.85, width = 0.85 },
   slack_horizontal = { top = 0.037, left = 0.028, height = 0.871, width = 0.459 },
   spotify = { top = 0.463, left = 0.013, height = 0.500, width = 0.426 },
   obsidian_lptp = { top = 0.145, left = 0.17, height = 0.82, width = 0.8 },
   obsidian_lptp_ext = { top = 0.042, left = 0.007, height = 0.941, width = 0.978 },
-  obsidian_horizontal = { top = 0.353, left = 0.500, height = 0.628, width = 0.483 },
+  obsidian_horiz = { top = 0.353, left = 0.500, height = 0.628, width = 0.483 },
   messages = { top = 0.452, left = 0.448, height = 0.535, width = 0.543 },
   citrix = {
     msg = { top = 0.078, left = 0.007, height = 0.656, width = 0.486 },
@@ -34,20 +33,20 @@ hs.hotkey.bind(bind_keys, "w", function()
   s.moveIfOpen("Spotify", "HORIZONTAL", ratios.spotify)
   s.moveIfOpen("bbvpn2", "HORIZONTAL", ratios.bbvpn)
   s.moveIfOpen("Calendar", "HORIZONTAL", ratios.calendar)
-  s.moveIfOpen("Obsidian", "HORIZONTAL", ratios.obsidian_horizontal)
+  s.moveIfOpen("Obsidian", "HORIZONTAL", ratios.obsidian_horiz)
   s.moveIfOpen("Slack", "HORIZONTAL", ratios.slack_horizontal)
   s.moveIfOpen("Messages", "LAPTOP", ratios.messages)
   s.moveIfOpen(
     "WezTerm",
-    external_connected and "VERTICAL" or "LAPTOP",
-    ratios.wezterm,
+    "LAPTOP",
+    external_connected and ratios.obsidian_lptp_ext or ratios.obsidian_lptp,
     false,
     true
   )
   s.moveIfOpen(
     "Google Chrome",
-    external_connected and "VERTICAL" or "LAPTOP",
-    external_connected and ratios.chrome_vertical or ratios.chrome_horizontal,
+    external_connected and "HORIZONTAL" or "LAPTOP",
+    external_connected and ratios.chrome_horiz or ratios.chrome_ext_lptp,
     false,
     true
   )
@@ -58,35 +57,20 @@ hs.hotkey.bind(bind_keys, "r", function()
   hs.alert.show("Hammerspoon config reloaded")
 end)
 
--- Size and place chrome (llm) on external monitor
+-- Secondary chrome window
 hs.hotkey.bind(bind_keys, "c", function()
   s.moveIfOpen(
     "Google Chrome",
     external_connected and "HORIZONTAL" or "LAPTOP",
-    external_connected and ratios.chrome_horizontal or ratios.chrome_lptp,
+    external_connected and ratios.chrome_ext_lptp or ratios.chrome_lptp,
     false,
     true
   )
 end)
 
--- Size and place chrome on laptop
-hs.hotkey.bind(bind_keys, "l", function()
-  s.moveIfOpen("Google Chrome", "LAPTOP", ratios.chrome_lptp, false, true)
-end)
-
--- Terminal for notes on laptop
-hs.hotkey.bind(bind_keys, "o", function()
-  s.moveIfOpen(
-    "WezTerm",
-    "LAPTOP",
-    external_connected and ratios.obsidian_lptp_ext or ratios.obsidian_lptp,
-    false
-  )
-end)
-
 -- Fullscreen terminal for text editing on external monitor
 hs.hotkey.bind(bind_keys, "s", function()
-  s.moveIfOpen("WezTerm", "HORIZONTAL", nil, true, true)
+  s.moveIfOpen("WezTerm", "VERTICAL", nil, true, true)
 end)
 
 -- Print app name and top/left/height/width of the focused window to console
