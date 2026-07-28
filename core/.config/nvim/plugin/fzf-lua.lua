00a73486d57a9c7f6cdb05f90f9f8599e89b2b42
Derek Huang <derekh919@gmail.com>
2026-07-28 17:54:26 -0400

    nvim [fzf-lua]: allow rg to search hidden files in dotfiles

--- core/.config/nvim/plugin/fzf-lua.lua
+++ core/.config/nvim/plugin/fzf-lua.lua
@@ -22,6 +22,20 @@ fzflua.setup({
   winopts = { preview = { layout = "vertical" } },
 })
 
+local dotfiles_dir = vim.fn.expand("~/dotfiles")
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+  desc = "Search hidden files with fzf-lua grep while in dotfiles",
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    local in_dotfiles = cwd == dotfiles_dir or vim.startswith(cwd, dotfiles_dir .. "/")
+    -- do_not_reset_defaults=true merges onto the setup() above instead of
+    -- wiping its actions/winopts back to fzf-lua's built-in defaults
+    fzflua.setup({
+      grep = { hidden = in_dotfiles },
+    }, true)
+  end,
+})
+
 k.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "fzf buffers" })
 k.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "fzf files" })
 k.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "fzf live grep" })
